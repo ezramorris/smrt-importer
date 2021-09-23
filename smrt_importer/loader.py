51e1366b9e235a93262f4208d501297449caa680
Ezra Morris <git@ezramorris.co.uk>
2021-09-23 18:54:29 +0100

    Improve validation and error handling of header

--- smrt_importer/loader.py
+++ smrt_importer/loader.py
@@ -4,37 +4,71 @@ from enum import Enum
 import re
 
 
-SMRT_FILE_TYPE = 'SMRT'
-COMPANY_ID = 'GAZ'
-GEN_NUM_FORMAT = re.compile('(PN|DV)[0-9]{6}')
+class Field:
+    """Holds information about a CSV field."""
+
+    def __init__(self, name, format) -> None:
+        """name: field name
+        format: regular expression which field values must fully match
+        """
+
+        self.name = name
+        self.format = format
+
+    @property
+    def format(self):
+        return self._format
+
+    @format.setter
+    def format(self, format):
+        self._format = re.compile(format)
 
 
 SMRTFileInfo = namedtuple('SMRTFileInfo', 'timestamp gen_num')
 
 
-class RecordType(Enum):
-    header = 'HEADR'
+class DecodingError(Exception):
+    pass
+
+
+HEADER_FIELDS = [
+    Field('record_type', 'HEADR'),
+    Field('file_type', 'SMRT'),
+    Field('company_id', 'GAZ'),
+    Field('date_str', '[0-9]{8}'),
+    Field('time_str', '[0-9]{6}'),
+    Field('gen_num', '(PN|DV)[0-9]{6}')
+]
 
 
 class SMRTLoader:
-    def process_header(self, header_fields):
-        record_type, file_type, company_id, date_str, time_str, gen_num = header_fields
-        
-        # TODO: better handling of unexpected values.
-        assert record_type == RecordType.header.value
-        assert file_type == SMRT_FILE_TYPE
-        assert company_id == COMPANY_ID
-        assert len(date_str) == 8
-        assert len(time_str) == 6
-        assert re.fullmatch(GEN_NUM_FORMAT, gen_num) is not None
+    def _validate_field(self, field: Field, value):
+        if re.fullmatch(field.format, value) is None:
+            raise DecodingError(f'invalid {field.name} value: {value}')
 
-        timestamp = datetime(
-            year=int(date_str[0:4]),
-            month=int(date_str[4:6]),
-            day=int(date_str[6:8]),
-            hour=int(time_str[0:2]),
-            minute=int(time_str[2:4]),
-            second=int(time_str[4:6])
-        )
+    def process_header(self, header_values: list):
+        if len(header_values) != len(HEADER_FIELDS):
+            raise DecodingError('invalid number of header fields')
 
-        return SMRTFileInfo(timestamp, gen_num)
+        values = {}
+        for field, value in zip(HEADER_FIELDS, header_values):
+            self._validate_field(field, value)
+            values[field.name] = value
+
+        date_str, time_str = values['date_str'], values['time_str']
+
+        # It's still possible to fail at this point - we haven't validated
+        # the date and time fields represent a valid timestamp.
+        try:
+            timestamp = datetime(
+                year=int(date_str[0:4]),
+                month=int(date_str[4:6]),
+                day=int(date_str[6:8]),
+                hour=int(time_str[0:2]),
+                minute=int(time_str[2:4]),
+                second=int(time_str[4:6])
+            )
+        except ValueError as e:
+            raise DecodingError(f'failed to parse timestamp: {e}')
+
+        return SMRTFileInfo(timestamp, values['gen_num'])
--- tests/test_smrt_importer/test_loader.py
+++ tests/test_smrt_importer/test_loader.py
@@ -1,19 +1,61 @@
 from datetime import datetime
 from unittest import TestCase
 
-from smrt_importer.loader import SMRTLoader
+from smrt_importer.loader import SMRTLoader, DecodingError
 
 
 VALID_HEADER = ['HEADR', 'SMRT', 'GAZ', '20210102', '135821', 'PN123456']
 
 
 class ProcessHeaderTestCase(TestCase):
+    def test_not_enough_fields(self):
+        loader = SMRTLoader()
+        header = VALID_HEADER.copy()
+        del header[-1]
+        with self.assertRaises(DecodingError):
+            loader.process_header(header)
+
+    def test_too_many_fields(self):
+        loader = SMRTLoader()
+        header = VALID_HEADER.copy()
+        header.append('foo')
+        with self.assertRaises(DecodingError):
+            loader.process_header(header)
+
+    def test_invalid_record_type(self):
+        loader = SMRTLoader()
+        header = VALID_HEADER.copy()
+        header[0] = 'FOO'
+        with self.assertRaises(DecodingError):
+            loader.process_header(header)
+
     def test_parse_valid_timestamp(self):
         loader = SMRTLoader()
         info = loader.process_header(VALID_HEADER)
         self.assertEqual(info.timestamp, datetime(2021, 1, 2, 13, 58, 21))
 
+    def test_parse_invalid_date(self):
+        loader = SMRTLoader()
+        header = VALID_HEADER.copy()
+        header[3] = '20211302'
+        with self.assertRaises(DecodingError):
+            loader.process_header(header)
+
+    def test_parse_invalid_time(self):
+        loader = SMRTLoader()
+        header = VALID_HEADER.copy()
+        header[4] = '605821'
+        with self.assertRaises(DecodingError):
+            loader.process_header(header)
+
     def test_parse_valid_gen_num(self):
         loader = SMRTLoader()
         info = loader.process_header(VALID_HEADER)
         self.assertEqual(info.gen_num, 'PN123456')
+
+    def test_parse_invalid_gen_num(self):
+        loader = SMRTLoader()
+        header = VALID_HEADER.copy()
+        header[5] = 'ABCDEFGH'
+        with self.assertRaises(DecodingError):
+            loader.process_header(header)
