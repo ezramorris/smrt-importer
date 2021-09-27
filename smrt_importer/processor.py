7a632f535b6aff817f8527a86ccaa026a0623295
Ezra Morris <git@ezramorris.co.uk>
2021-09-27 22:30:30 +0100

    Add docstring to process_file

--- smrt_importer/processor.py
+++ smrt_importer/processor.py
@@ -6,6 +6,11 @@ from smrt_importer.db import insert_file
 
 
 def process_file(path):
+    """Load data from a single file and save to DB.
+    
+    path: path (string or Path object) to a CSV file.
+    """
+
     loader = SMRTLoader()
     file = loader.load_file(path)
     insert_file(file)
